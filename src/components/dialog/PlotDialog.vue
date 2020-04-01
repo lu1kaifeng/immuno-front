e094a25cf1cdfc2d4bf1d4e931edb7af7c99ecf9
ChengengLu <qq844303749@gmail.com>
2020-04-01 09:13:23 +0800

    add table

--- src/components/dialog/PlotDialog.vue
+++ src/components/dialog/PlotDialog.vue
@@ -1,11 +1,41 @@
 <template>
         <v-dialog
                 v-model="display"
-                width="500"
         >
             <v-card>
                 <v-card-title class="headline"></v-card-title>
-
+                    <v-simple-table dense>
+                        <template v-slot:default>
+                            <thead>
+                            <tr>
+                                <th class="text-left">Sample</th>
+                                <th class="text-left">Gene</th>
+                                <th class="text-left">Genomic Region</th>
+                                <th class="text-left">Methylation Position</th>
+                                <th class="text-left">Relation To Island</th>
+                                <th class="text-left">TNM</th>
+                                <th class="text-left">Gender</th>
+                                <th class="text-left">Race</th>
+                                <th class="text-left">Survival</th>
+                                <th class="text-left">Split</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>{{ entry.sample }}</td>
+                                <td>{{ entry.gene }}</td>
+                                <td>{{ entry.genomicRegion }}</td>
+                                <td>{{ entry.methylationPosition }}</td>
+                                <td>{{ entry.relationToIsland }}</td>
+                                <td>{{ entry.tnm===null?'All':entry.tnm }}</td>
+                                <td>{{ entry.gender===null?'All':entry.gender  }}</td>
+                                <td>{{ entry.race===null?'All':entry.race  }}</td>
+                                <td>{{ entry.survival }}</td>
+                                <td>{{ entry.split }}</td>
+                            </tr>
+                            </tbody>
+                        </template>
+                    </v-simple-table>
                 <v-card-text >
                     <PlotSection v-bind:id="entry.id" v-bind:entry="entry" v-bind:re-render="reRender"/>
                 </v-card-text>
